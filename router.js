73159e280427f1d4630fc518e36d4f6c81d1c9fd
jsivakumaran <jsivakumarn@outlook.com>
2015-03-02 14:01:14 -0800

    Add ember route files, image carousel

---
+++ router.js
@@ -0,0 +1,10 @@
+Bustle.Router.map(function() {
+  this.resource('home', {path: '/'});
+  this.resource('clink1');
+  this.resource('clink2');
+  this.resource('clink3');
+  this.resource('clink4');
+  this.resource('entertainment');
+  this.resource('news');
+  this.resource('lifestyle');
+});
